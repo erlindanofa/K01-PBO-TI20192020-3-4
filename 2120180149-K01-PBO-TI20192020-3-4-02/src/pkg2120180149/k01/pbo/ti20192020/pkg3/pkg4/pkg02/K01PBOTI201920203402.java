d8a59508ddd5bd4102dac18a8e859988a354efdc
erlindanofa <56105084+erlindanofa@users.noreply.github.com>
2019-10-25 21:24:07 +0700

    Upload Tugas K01

---
+++ 2120180149-K01-PBO-TI20192020-3-4-02/src/pkg2120180149/k01/pbo/ti20192020/pkg3/pkg4/pkg02/K01PBOTI201920203402.java
@@ -0,0 +1,35 @@
+package 2120180149.k01.pbo.ti20192020.3.4.02;
+public class 2120180149K01PBOTI201920203402 {
+    public static void main(String[] args) {
+    //instansiasi dari kelas biodata mahasiswa
+    mahasiswa mhs_erlinda = new mahasiswa ();
+    mhs_erlinda.Alamat="Sukorejo";//akses variabel biodata Alamat
+    System.out.print ("Alamat mahasiswa erlinda : "+mhs_erlinda.Alamat);
+    mhs_erlinda.NIM="2120180149";//akses variabel biodata NIM
+    System.out.print ("NIM mahasiswa erlinda : "+mhs_erlinda.NIM);
+    
+    mahasiswa mhs_diah = new mahasiswa ();
+    mhs_diah.Alamat="Sumbang Timun";//akses variabel biodata Alamat
+    System.out.print ("Alamat mahasiswa diah : "+mhs_diah.Alamat);
+    mhs_diah.NIM="2120180130";//akses variabel biodata NIM
+    System.out.print ("NIM mahasiswa diah : "+mhs_diah.NIM);
+    
+    mahasiswa mhs_arini = new mahasiswa ();
+    mhs_arini.Alamat="Sukorejo";//akses variabel biodata Alamat
+    System.out.print ("Alamat mahasiswa arini : "+mhs_arini.Alamat);
+    mhs_arini.NIM="2120180144";//akses variabel biodata NIM
+    System.out.print ("NIM mahasiswa arini : "+mhs_arini.NIM);
+    
+    mahasiswa mhs_erlin = new mahasiswa ();
+    mhs_erlin.Alamat="Parengan Tuban";//akses variabel biodata Alamat 
+    System.out.print ("Alamat mahasiswa erlin : "+mhs_erlin.Alamat);
+    mhs_erlin.NIM="2120180140";//akses variabel biodata NIM
+    System.out.print ("NIM mahasiswa erlin : "+mhs_erlin.NIM);
+    
+    mahasiswa mhs_yani = new mahasiswa ();
+    mhs_yani.Alamat="Sumengko Kalitidu";//akses variabel biodata Alamat
+    System.out.print ("Alamat mahasiswa yani : "+mhs_yani.Alamat);
+    mhs_yani.NIM="2120180150";//akses variabel biodata NIM
+    System.out.print ("NIM mahasiswa yani : "+mhs_yani.NIM);
+    }
+}
